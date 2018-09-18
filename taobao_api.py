595b0dad2c2a6760a67272c69a790fd0c5944b59
second <178344859@qq.com>
2018-09-18 19:08:05 +0800

    update project

--- taobao_api.py
+++ taobao_api.py
@@ -11,8 +11,8 @@ api_search_url = 'https://s.taobao.com/search?q=%s'
 
 line_tag = "    g_page_config = "
 
-def main():
-	html = requests.get(api_search_url % ("电钻")).text
+def get_page_config(word):
+	html = requests.get(api_search_url % (word)).text
 	g_page_config_str = None
 	for line in html.split("\n"):
 		# line = line.encode("utf-8")
@@ -21,12 +21,23 @@ def main():
 
 			print(line)
 			break
+	return g_page_config_str
+	pass
+
+def get_node(url, tag):
+	html = requests.get(url).text
+	pass
+
+def main():
+	g_page_config_str = get_page_config("电钻")
 	g_page_config_obj = json.loads(g_page_config_str)
 	auctions = g_page_config_obj["mods"]["itemlist"]["data"]["auctions"]
 	for auction_obj in auctions:
 		print(auction_obj["title"])
 		print(auction_obj["raw_title"])
 		print(auction_obj["detail_url"])
+		detail_url = "http:" + auction_obj["detail_url"]
+
 	pass
 
 if __name__ == '__main__':
